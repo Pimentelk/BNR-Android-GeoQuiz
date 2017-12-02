8334b7b909bf0a1fc444aa82429249e7b1d3894a
Kevin Pimentel <kevin@gymautopilot.com>
2017-12-02 18:39:44 -0500

    GeoQuiz now gives you a % score when quiz is completed. Added isAnswered method. Added setAnswered method.

--- app/src/main/java/com/bignerdranch/android/geoquiz/Question.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/Question.java
@@ -7,11 +7,13 @@ package com.bignerdranch.android.geoquiz;
 public class Question {
     private int mTextResId;
     private boolean mAnswerTrue;
+    private boolean mAnswered;
 
     public Question(int textResId, boolean answerTrue)
     {
         mTextResId = textResId;
         mAnswerTrue = answerTrue;
+        mAnswered = false;
     }
 
     public int getTextResId() {
@@ -28,5 +30,13 @@ public class Question {
 
     public void setAnswerTrue(boolean answerTrue) {
         mAnswerTrue = answerTrue;
+    }
+
+    public boolean isAnswered() {
+        return mAnswered;
+    }
+
+    public void setAnsweredTrue(boolean answered) {
+        mAnswered = answered;
     }
 }
